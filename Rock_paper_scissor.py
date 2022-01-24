5fbc038bf07fcf7ef3d4d7e6536f4f54749e5a78
jojo <vitaldev310@gmail.com>
2022-01-24 19:57:50 +0530

    1st Loop added to continue game as your wish :)

--- Rock_paper_scissor.py
+++ Rock_paper_scissor.py
@@ -31,92 +31,104 @@ no = random.randint(1,3)
 
 
 
+s = True
+while s ==True:
 
 
-m = input(" Choose : Single Player(S) or Multiplayer(M)  [*CASE SESNITIVE*] ? ")
+    m = input(" Choose : Single Player(S) or Multiplayer(M)  [*CASE SESNITIVE*] ? ")
 
 
 
-if m == 'S':
-    
-
-    Comp = print(" Comp Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
-    
-    
-    
-    if no == 1:
-        Comp = "s"
-    elif no == 2:
-        Comp = "p"
-    else :
-        Comp = "r"
-    
-    
-    b = input(" YOU Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
-    
-    
-    e = game(Comp,b)
-    
-    print(f"Comp :- {Comp}")
-    print(f"You :- {b}")
-    
-    if e == None:
-        print("Draw!")
-
-
-    elif e :
-        print("You Win!")
-
-    else:
-        print("Comp Wins!")
-
-    
-
-
-   
-elif m =="M":
-
-
-    a = input (" P1 Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
+    if m == 'S':
         
-    b = input(" P2 Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
-    
-  
-    
-  
-    
-    
-    e = game(a,b)
-    
-    print(f"P1 :- {a}")
-    print(f"P2 :- {b}")
-    
-    if e == None:
-        print("Draw!")
+
+        Comp = print(" Comp Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
+        
+        
+        
+        if no == 1:
+            Comp = "s"
+        elif no == 2:
+            Comp = "p"
+        else :
+            Comp = "r"
+        
+        
+        b = input(" YOU Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
+        
+        
+        e = game(Comp,b)
+        
+        print(f"Comp :- {Comp}")
+        print(f"You :- {b}")
+        
+        if e == None:
+            print("Draw!")
 
 
-    elif e :
-        print("P2 Wins!")
+        elif e :
+            print("You Win!")
+
+        else:
+            print("Comp Wins!")
+
+        
+
+
+    
+    elif m =="M":
+
+
+        a = input (" P1 Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
+            
+        b = input(" P2 Choose : Rock(r) , Paper(p) or Scissor(s) ? ")
+        
+    
+        
+    
+        
+        
+        e = game(a,b)
+        
+        print(f"P1 :- {a}")
+        print(f"P2 :- {b}")
+        
+        if e == None:
+            print("Draw!")
+
+
+        elif e :
+            print("P2 Wins!")
+
+        else:
+            print("P1 Wins!")
+
+        
+
 
     else:
-        print("P1 Wins!")
+        print("CHOOSE THE GODDAMM WORD ABOVE YOU BLIND MF")
 
-    
+    h = True
+    while h == True:
 
+        
+        g = input('''Do You want to play again
+            Press (y) for yes
+            Press (n) for no\n''')    
 
-else:
-    print("CHOOSE THE GODDAMM WORD ABOVE YOU BLIND MF")
-
-
-    
-           
-            
-
-
-
-
-
-
+        if g == 'y':
+            print ("Ok")
+            s = True
+            h = False
+        elif g == 'n':
+            print ('BHAG')
+            s = False
+            h = False
+        else:
+            print("YoU reALly bLiND nEGGa") 
+            s = False
+            h = True
 
 
 
